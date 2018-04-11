ebd9c4632af6b8c5899e96aeba43c9f70a82ba1b
Geng Zhang <zhanggeng.zg@antfin.com>
2018-04-11 16:23:21 +0800

    Update test case.

--- src/test/java/com/alipay/hessian/generic/test/PersonTest.java
+++ src/test/java/com/alipay/hessian/generic/test/PersonTest.java
@@ -57,19 +57,19 @@ public class PersonTest {
 
     @Test
     public void testWriteDefinitionWithSameDefinition() throws Exception {
-        WRITE_DEFINITION_EVERYTIME_FIELD.set(GenericObjectSerializer.class, true);
+        setWriteDefinitionEverytimeField(true);
         testSameDefinition();
     }
 
     @Test
     public void testNotWriteDefinitionWithSameDefinition() throws Exception {
-        WRITE_DEFINITION_EVERYTIME_FIELD.set(GenericObjectSerializer.class, false);
+        setWriteDefinitionEverytimeField(false);
         testSameDefinition();
     }
 
     @Test
     public void testWriteDefinitionWithDiffDefinition() throws Exception {
-        WRITE_DEFINITION_EVERYTIME_FIELD.set(GenericObjectSerializer.class, true);
+        setWriteDefinitionEverytimeField(true);
         testDiffDefinition();
     }
 
@@ -77,7 +77,7 @@ public class PersonTest {
     public void testNotWriteDefinitionWithDiffDefinition() throws Exception {
         boolean hasException = false;
         try {
-            WRITE_DEFINITION_EVERYTIME_FIELD.set(GenericObjectSerializer.class, false);
+            setWriteDefinitionEverytimeField(false);
             testDiffDefinition();
         } catch (Throwable t) {
             hasException = true;
@@ -143,59 +143,76 @@ public class PersonTest {
     }
 
     public void testDiffDefinition() throws Exception {
-        GenericObject go1 = new GenericObject(Person.class.getName());
-        go1.putField("name", "zhangsan");
-        //        go1.putField("age", 12);
-        go1.putField("gender", null);
-        go1.putField("scores", new HashMap<String, Integer>());
-        go1.putField("friend", null);
-        go1.putField("pet", GenericUtils.convertToGenericObject(new Pet()));
-        //        go1.putField("mapValue", null);
-        go1.putField("listValue", new ArrayList<Object>());
+        try {
+            GenericObject go1 = new GenericObject(Person.class.getName());
+            go1.putField("name", "zhangsan");
+            //        go1.putField("age", 12);
+            go1.putField("gender", null);
+            go1.putField("scores", new HashMap<String, Integer>());
+            go1.putField("friend", null);
+            go1.putField("pet", GenericUtils.convertToGenericObject(new Pet()));
+            //        go1.putField("mapValue", null);
+            go1.putField("listValue", new ArrayList<Object>());
 
-        GenericObject go2 = new GenericObject(Person.class.getName());
-        go2.putField("name", "lisi");
-        go2.putField("age", 21);
-        //        go2.putField("gender", "Femal");
-        go2.putField("scores", new HashMap<String, Integer>());
-        go2.putField("friend", GenericUtils.convertToGenericObject(new Person()));
-        //        go2.putField("pet", null);
-        go2.putField("mapValue", null);
-        go2.putField("listValue", null);
+            GenericObject go2 = new GenericObject(Person.class.getName());
+            go2.putField("name", "lisi");
+            go2.putField("age", 21);
+            //        go2.putField("gender", "Femal");
+            go2.putField("scores", new HashMap<String, Integer>());
+            go2.putField("friend", GenericUtils.convertToGenericObject(new Person()));
+            //        go2.putField("pet", null);
+            go2.putField("mapValue", null);
+            go2.putField("listValue", null);
 
-        ByteArrayOutputStream bout = new ByteArrayOutputStream();
-        Hessian2Output hout = new Hessian2Output(bout);
-        hout.setSerializerFactory(new GenericSerializerFactory());
+            ByteArrayOutputStream bout = new ByteArrayOutputStream();
+            Hessian2Output hout = new Hessian2Output(bout);
+            hout.setSerializerFactory(new GenericSerializerFactory());
 
-        hout.writeObject(go1);
-        hout.writeObject(go2);
+            hout.writeObject(go1);
+            hout.writeObject(go2);
 
-        hout.close();
+            hout.close();
 
-        byte[] body = bout.toByteArray();
+            byte[] body = bout.toByteArray();
 
-        ByteArrayInputStream bin = new ByteArrayInputStream(body, 0, body.length);
-        Hessian2Input hin = new Hessian2Input(bin);
-        hin.setSerializerFactory(new SerializerFactory());
+            ByteArrayInputStream bin = new ByteArrayInputStream(body, 0, body.length);
+            Hessian2Input hin = new Hessian2Input(bin);
+            hin.setSerializerFactory(new SerializerFactory());
 
-        Person p1 = (Person) hin.readObject();
-        assertEquals("zhangsan", p1.getName());
-        //        assertEquals(12, p1.getAge());
-        assertEquals(null, p1.getGender());
-        assertEquals(0, p1.getScores().size());
-        assertEquals(null, p1.getFriend());
-        assertNotNull(p1.getPet());
-        //        assertEquals(null, p1.getMapValue());
-        assertEquals(0, p1.getListValue().size());
+            Person p1 = (Person) hin.readObject();
+            assertEquals("zhangsan", p1.getName());
+            //        assertEquals(12, p1.getAge());
+            assertEquals(null, p1.getGender());
+            assertEquals(0, p1.getScores().size());
+            assertEquals(null, p1.getFriend());
+            assertNotNull(p1.getPet());
+            //        assertEquals(null, p1.getMapValue());
+            assertEquals(0, p1.getListValue().size());
 
-        Person p2 = (Person) hin.readObject();
-        assertEquals("lisi", p2.getName());
-        assertEquals(21, p2.getAge());
-        //        assertEquals("Femal", p2.getGender());
-        assertEquals(0, p2.getScores().size());
-        assertNotNull(p2.getFriend());
-        //        assertEquals(null, p2.getPet());
-        assertEquals(null, p2.getMapValue());
-        assertEquals(null, p2.getListValue());
+            Person p2 = (Person) hin.readObject();
+            assertEquals("lisi", p2.getName());
+            assertEquals(21, p2.getAge());
+            //        assertEquals("Femal", p2.getGender());
+            assertEquals(0, p2.getScores().size());
+            assertNotNull(p2.getFriend());
+            //        assertEquals(null, p2.getPet());
+            assertEquals(null, p2.getMapValue());
+            assertEquals(null, p2.getListValue());
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println(Thread.currentThread().getName() + "current WriteDefinitionEverytimeField to "
+                    + WRITE_DEFINITION_EVERYTIME_FIELD.get(null));
+            throw e;
+        }
+        
     }
+    
+    private void setWriteDefinitionEverytimeField(boolean write){
+        try {
+            System.out.println(Thread.currentThread().getName() + "setWriteDefinitionEverytimeField to " + write);
+            WRITE_DEFINITION_EVERYTIME_FIELD.set(GenericObjectSerializer.class, write);
+        } catch (IllegalAccessException e) {
+        }
+    } 
+    
 }
