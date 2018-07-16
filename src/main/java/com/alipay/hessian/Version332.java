08c6d3d7849d215c21be2a38aa70f1c751946708
leizhiyuan <leizhiyuan@gmail.com>
2018-07-16 13:26:06 +0800

    feat: update hessian version 3.3.2

---
+++ src/main/java/com/alipay/hessian/Version332.java
@@ -0,0 +1,14 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2018 All Rights Reserved.
+ */
+package com.alipay.hessian;
+
+/**
+ * only for security hack scan. do not delete it
+ * every publish you need to change class name
+ * @author bystander
+ * @version $Id: Version326.java, v 0.1 2018年07月06日 5:21 PM bystander Exp $
+ */
+public class Version332 {
+}
