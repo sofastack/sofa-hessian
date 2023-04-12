0ad2bbecdda5b322fa42645384f7f9e56a6c3409
均源 <zhangminglun.zml@antgroup.com>
2023-04-12 16:40:01 +0800

    fix: log

--- src/main/java/com/caucho/hessian/util/ReflectionUtil.java
+++ src/main/java/com/caucho/hessian/util/ReflectionUtil.java
@@ -4,11 +4,11 @@
  */
 package com.caucho.hessian.util;
 
+import org.slf4j.Logger;
+
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -16,14 +16,35 @@ import java.util.logging.Logger;
  * @version ReflectionUtil.java, v 0.1 2023年03月30日 21:23 junyuan Exp $
  */
 public class ReflectionUtil {
-    private static final Logger log = Logger.getLogger(ReflectionUtil.class.getName());
+    private static org.slf4j.Logger LOGGER                     = judgeLogger();
+
+    //do not change this
+    public static final String      HESSIAN_SERIALIZE_LOG_NAME = "HessianSerializeLog";
+    public static final String      CONFIG_LOG_SPACE_NAME      = "com.alipay.sofa.hessian";
+
+    private static Logger judgeLogger() {
+
+        try {
+            ReflectionUtil.class.getClassLoader().loadClass("com.alipay.sofa.common.log.LoggerSpaceManager");
+        } catch (Throwable e) {
+            //do nothing
+            return null;
+        }
+
+        return com.alipay.sofa.common.log.LoggerSpaceManager.getLoggerBySpace(HESSIAN_SERIALIZE_LOG_NAME,
+            CONFIG_LOG_SPACE_NAME);
+    }
 
     public static boolean setAccessible(Method m) {
         try {
             m.setAccessible(true);
         } catch (Throwable t) {
-            log.log(Level.INFO,
-                "failed when setting accessible on method [" + m.toString() + "], error message: " + t.getMessage(), t);
+            if (LOGGER.isDebugEnabled()) {
+                LOGGER
+                    .debug(
+                        "failed when setting accessible on method [" + m.toString() + "], error message: " +
+                            t.getMessage(), t);
+            }
             return false;
         }
         return true;
@@ -33,8 +54,12 @@ public class ReflectionUtil {
         try {
             c.setAccessible(true);
         } catch (Throwable t) {
-            log.log(Level.INFO, "failed when setting accessible on constructor [" + c.toString() +
-                "], error message: " + t.getMessage(), t);
+            if (LOGGER.isDebugEnabled()) {
+                LOGGER
+                    .debug(
+                        "failed when setting accessible on method [" + c.toString() + "], error message: " +
+                            t.getMessage(), t);
+            }
             return false;
         }
         return true;
@@ -44,8 +69,12 @@ public class ReflectionUtil {
         try {
             f.setAccessible(true);
         } catch (Throwable t) {
-            log.log(Level.INFO,
-                "failed when setting accessible on field [" + f.toString() + "], error message: " + t.getMessage(), t);
+            if (LOGGER.isDebugEnabled()) {
+                LOGGER
+                    .debug(
+                        "failed when setting accessible on method [" + f.toString() + "], error message: " +
+                            t.getMessage(), t);
+            }
             return false;
         }
         return true;
