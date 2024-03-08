80307c1d2679e54a88aee8fab0bdb38f121efcb7
lo1nt <zhangminglun.zml@antgroup.com>
2024-03-08 17:38:51 +0800

    chore: format

--- src/test/java/com/caucho/hessian/io/CompatibleTest.java
+++ src/test/java/com/caucho/hessian/io/CompatibleTest.java
@@ -1,6 +1,18 @@
 /*
- * Ant Group
- * Copyright (c) 2004-2024 All Rights Reserved.
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.caucho.hessian.io;
 
@@ -19,9 +31,9 @@ import java.io.IOException;
  */
 public class CompatibleTest {
 
-    Throwable                            t               = null;
+    Throwable                            t = null;
     private static ByteArrayOutputStream os;
-    private static SerializerFactory factory;
+    private static SerializerFactory     factory;
 
     @BeforeClass
     public static void setUp() {
